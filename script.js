aef767ac3b3a106aad8346bfeab1283c5d7571f7
Aqilah <anakdinnaju@gmail.com>
2026-09-09 02:28:27 +0800

    update

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+new Swiper('.work-swiper', {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: '.work-next',
+    prevEl: '.work-prev',
+  },
+  breakpoints: {
+    561: { slidesPerView: 2 },
+    861: { slidesPerView: 3 },
+  },
+});
